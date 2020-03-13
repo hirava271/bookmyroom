9b9adefcf8ebd01012e3f5ed7ddca24c19c04147
Hirava Anaghan <hirava2701@gmail.com>
2020-03-13 14:50:12 -0400

    create nest project. change project structure.

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <label htmlFor="selectedDate">Select Date:</label>
+            <input type="date" name="selectedDate" />
+        </div>
+    );
+}
+
+export default App;
