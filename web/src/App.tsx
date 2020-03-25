ecefabeb352b25b2dfb83967fa87a70a7aaa5d04
Hirava Anaghan <hirava2701@gmail.com>
2020-03-24 20:36:55 -0400

    add date and time picker.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,33 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 // eslint-disable-next-line no-unused-vars
-import axios from 'axios';
 // eslint-disable-next-line no-unused-vars
-import { Room } from '../../api/src/rooms/interfaces/room.interface';
+import {
+    DatePickerComponent,
+    TimePickerComponent,
+} from '@syncfusion/ej2-react-calendars';
+import './index.css';
 
 function App(): JSX.Element {
-    const [rooms, setRooms] = useState<Room[]>([]);
+    // const [rooms, setRooms] = useState<Room[]>([]);
 
-    useEffect(() => {
-        axios.get<Room[]>('/api/rooms').then(response => {
-            setRooms(response.data);
-        });
-    }, []);
+    // useEffect(() => {
+    //     axios.get<Room[]>('/api/rooms').then(response => {
+    //         setRooms(response.data);
+    //     });
+    // }, []);
 
     return (
         <div>
-            <div>
-                <label htmlFor="selectedDate">Select Date:</label>
-                <input type="date" name="selectedDate" />
+            <div className="datetime-selector">
+                <span>Meeting Timing:</span>
+                <div className="datepicker-control-section">
+                    <DatePickerComponent />
+                </div>
+                <div className="datetime-selector">
+                    <div className="time-picker-control-section">
+                        <TimePickerComponent />
+                    </div>
+                    <span>to</span>
+                    <div className="time-picker-control-section">
+                        <TimePickerComponent />
+                    </div>
+                </div>
             </div>
-
-            {rooms.length > 0 &&
-                rooms.map((room: Room, index: number) => {
-                    return (
-                        <div key={index}>
-                            <span>{room.name}</span>
-                        </div>
-                    );
-                })}
         </div>
     );
 }
